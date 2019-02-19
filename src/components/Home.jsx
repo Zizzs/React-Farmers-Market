522e57438aa9ed401b630e972b030feba3b3d8ac
Alex Williams and Gulzat Karimova <gulzat@>
2019-02-19 15:09:32 -0800

    Added jsx styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import local from '../assets/images/local.jpeg';
 
 
 function Home(){
   return (
     <div>
-      <p>Test</p>
+       <style jsx> {`
+    h1 {
+      text-align: center;
+      font-size: 45px;
+    }
+    `}</style>
+      <h1> Farmers Market</h1>
+      <img src={local} />
     </div>
   );
 }
